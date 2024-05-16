1251c62a0c43a3c33485ed3e98405d41eca67c96
ggx <1056937524@qq.com>
2024-05-16 22:35:46 +0800

    add dynamic-import

--- packages/code-splitting/splitting-large-package/src/App.js
+++ packages/code-splitting/splitting-large-package/src/App.js
@@ -1,32 +1,19 @@
 import { useState } from "react";
-const Footer = React.lazy(() => import("./components/Footer.jsx"));
-const NavBar = React.lazy(
-  () =>
-    // 模拟网络延迟，延迟2秒加载组件
-    new Promise((resolve) =>
-      setTimeout(() => resolve(import("./components/Nav.jsx")), 2000)
-    )
-);
+import add from 'lodash/add';
 
 export default function App() {
   const [count, setCount] = useState("0");
 
   async function handleClick() {
-    setCount((prev) => ++prev);
+    setCount((prev) => add(prev, count));
   }
 
   return (
     <div>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <NavBar />
-      </React.Suspense>
       <main>
         <button onClick={() => handleClick()}>点击增加</button>
         <div>{count}</div>
       </main>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <Footer />
-      </React.Suspense>
     </div>
   );
 }
